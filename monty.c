0c0331aa26c8b33e102e780767698a8390a9b18c
Neo Masilo <neolawrencemasilo@gmail.com>
2023-08-22 21:11:06 +0000

    can open and read file, added tokenization

---
+++ monty.c
@@ -0,0 +1,136 @@
+#include "monty.h"
+
+/**
+ * main - Enter point
+ * @argc: commandline argument count
+ * @argv: array of commandline arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	char **op_av, *buffer, **av;
+	int i = 0;
+	(void) argc;
+
+	if (argv[1] != NULL)
+	{
+		buffer = _read(argv);
+		op_av = tokenize(buffer, "$\n");
+		av = _token(op_av[0]);
+		while (av[i] != NULL)
+		{
+			printf("%s\n", av[i]);
+			i++;
+		}
+	}
+	free(buffer);
+	free(op_av);
+	return (0);
+}
+
+/**
+ * _token - tokenises a str
+ * @argv: the str
+ * Return: tokenised array
+ */
+char **_token(char *argv)
+{
+	char *token, **av;
+	int i = 0;
+
+	av = malloc(sizeof(char *) * sizeof(argv) + 1);
+	token = strtok(argv, " ");
+	while (token != NULL)
+	{
+		av[i] = token;
+		token = strtok(NULL, "\n");
+		i++;
+	}
+	av[i] = NULL;
+
+	return (av);
+}
+
+/**
+ * _read - opens a file and reads the file
+ * @argv: array of commandline arguments
+ * Return: the buffer with read content
+ */
+char *_read(char **argv)
+{
+	char *buffer;
+	int fd;
+	ssize_t readn;
+
+	buffer = malloc(sizeof(char *) * 1024);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		write(STDERR_FILENO, "Error: Can't open file ",
+				strlen("Error: Can't open file "));
+		printf("%s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	readn = read(fd, buffer, 1024);
+	if (readn == -1)
+	{
+		printf("read failed");
+		exit(EXIT_FAILURE);
+	}
+	return (buffer);
+}
+
+/**
+ * tokenize - tokenizes a the buffer
+ * @buffer: the buffer
+ * @delim: delimiter
+ * Return: double pointer array with the tokenized strings
+ */
+char **tokenize(char *buffer, char *delim)
+{
+	char *token = NULL, **op_av, buf_copy[1024], *token2 = NULL;
+	int cmd_count = 0, i = 0;
+
+	strcpy(buf_copy, buffer);
+	token = strtok(buffer, delim);
+	while (token != NULL)
+	{
+		token = strtok(NULL, delim);
+		cmd_count++;
+	}
+	op_av = malloc(sizeof(char *) * cmd_count + 1);
+	token2 = strtok(buf_copy, delim);
+	while (token2 != NULL)
+	{
+		op_av[i] = token2;
+		token2 = strtok(NULL, delim);
+		i++;
+	}
+	op_av[i] = NULL;
+	if (op_av == NULL)
+	{
+		free(op_av);
+		return (NULL);
+	}
+	return (op_av);
+}
+
+/**
+ * line_count - counts the number of commands
+ * @argv: array of commandline arguments
+ * Return: the count
+ */
+int line_count(char **argv)
+{
+	char *buffer, *token;
+	int count = 0;
+
+	 buffer = _read(argv);
+	token = strtok(buffer, "$\n");
+	while (token != NULL)
+	{
+		count++;
+		token = strtok(NULL, "$\n");
+	}
+	return (count);
+}
